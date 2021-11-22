e71833d5b7dd638591f440ca29e833b063b64306
Em <contactemilyhuang@gmail.com>
2021-11-22 15:15:57 -0800

    Post my answers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (hey,num) {
+    let repeatedHey = "";
+    
+    if(num < 0){
+        return "ERROR";
+    }
 
-};
+    for(let i = 0; i < num; i++){
+        //repeatedHey gets merged with hey and then repeatedHey's value gets updated
+        repeatedHey += hey;
+        }
+    //Need to return the updated repeatedHey value on each loop
+    return repeatedHey;
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
