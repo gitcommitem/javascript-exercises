09f9b7118e69aa16b03a2e28d1744ebed559ce14
Em <contactemilyhuang@gmail.com>
2021-11-24 05:42:52 -0800

    Add my answers

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...itemForRemoval) {
+    let itemRemovalLength = itemForRemoval.length;
+
+    for(let i = 0; i < itemRemovalLength; i++){
+        //If the item to be removed is not in the array, skip it
+        if (array.indexOf(itemForRemoval[i]) === -1){
+            continue
+        }
+        else{
+        //indexOf tells the splice what position to remove from
+        //1 tells the splice to only remove that one element
+        array.splice(array.indexOf(itemForRemoval[i]),1);
+        }
+    };
+
+    return array;
 
 };
 
