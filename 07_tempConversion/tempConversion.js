5f15b072c655150b6d6dacabc884c96ecc7a5087
Em <contactemilyhuang@gmail.com>
2021-11-24 13:06:25 -0800

    Post my answers

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fDegree) {
+  let cDegree = (fDegree - 32) * (5/9);
+  //toFixed returns as a string, so we use + to change it back to number
+  return +cDegree.toFixed(1);
 };
 
-const ctof = function() {
+const ctof = function(cDegree) {
+  let fDegree = (cDegree * (9/5)) + 32;
+  //toFixed returns as a string, so we use + to change it back to number
+  return +fDegree.toFixed(1);
 
 };
 
