be89d66059542a62f51522ae6e58eca8d73301fa
Em <contactemilyhuang@gmail.com>
2021-12-21 14:42:39 -0800

    Post my answers

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+let convertedString = string.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+let stringReverse = convertedString.split("").reverse().join("");
+if(convertedString === stringReverse){
+    return true
+}
+else{
+    return false
+}
 };
 
 // Do not edit below this line
