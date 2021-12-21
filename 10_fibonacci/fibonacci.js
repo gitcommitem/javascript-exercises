8879d582cc06733d7f9ff21b1c4087e8b9cf8f32
Em <contactemilyhuang@gmail.com>
2021-12-21 15:43:09 -0800

    Post my answers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+if(number < 0){
+    return "OOPS"
+}
+else{
+    let firstNum = 0;
+    let secondNum = 1;
+    
+    for(i=0; i < number ;i++){
+        sum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = sum;
+    }
+    
+    return firstNum
+}
 
 };
 
