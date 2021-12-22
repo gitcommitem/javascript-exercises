4dfc2653ae8bab0f20aa15d72de167732f10a4bf
Em <contactemilyhuang@gmail.com>
2021-12-21 16:26:59 -0800

    Post my answers

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayObject) {
+//from our object we want to assemble just the titles and return with an array that is JUST titles only
+return arrayObject.map(function(book){return book.title})
 };
 
 // Do not edit below this line
