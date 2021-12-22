c2fa1a2381f652d87bf75d2a811f0eab2459a2dc
Em <contactemilyhuang@gmail.com>
2021-12-22 13:15:28 -0800

    Post my answers

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayObject) {
+return arrayObject.reduce(function(firstPerson,secondPerson){
+let oldestPerson = getAge(firstPerson.yearOfBirth, firstPerson.yearOfDeath);
+let currentPerson = getAge(secondPerson.yearOfBirth, secondPerson.yearOfDeath);
+return (oldestPerson < currentPerson) ? secondPerson : firstPerson;
+})
+
+function getAge(birth,death){
+if(death === undefined){
+    death = new Date().getFullYear();
+}
+return death - birth;
+}
 
 };
 
